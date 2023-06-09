4f74ad36501557c4e6f736391cd6d9fcc7fd8f29
Souradeep <jeetsouradeep@gmail.com>
2023-06-08 20:27:28 -0400

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,261 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Database Engineer",
+    icon: web,
+  },
+  {
+    title: "Data Analyst",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Artificial Intelligence Engineer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Data Analyst Research Assistant",
+    company_name: "Cyberinfrastructure for Network Science (CNS) at Indiana University",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "August, 2022 - Present",
+    points: [
+      "Developed a data pipeline utilizing SPARQL and Python to extract data via REST API for performing comprehensive comparisons across 8 distinct organ datasets.",
+      "Performed Data Transformations between 150 thousand data points using Pandas to facilitate biological research.",
+      "Visualized the Data Points extracted from REST APIs using Neo4j to find hierarchical relationships in data and published the work in NIH website.",
+      "Implemented the BERT model to calculate scores for common keywords in biological names within a dataset, achieving an impressive accuracy of 93% through extensive analysis, fine-tuning the BERT for accurate keyword identification.",
+      "Implemented an end-to-end data pipeline using Apache Airflow to automate ETL processes, reducing manual effort by 80%.",
+    ],
+  },
+  {
+    title: "Machine Learning / Natural Language Processing Intern",
+    company_name: "Collab Lab Pvt. Ltd.",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "April, 2021 -  August, 2021",
+    points: [
+      "Developed a student-friendly chatbot module utilizing Transformers, incorporating a 3 million question corpus to facilitate engaging and informative conversations with students.",
+      "Collaborated  with the development team following AGILE methodology of SDLC (Software Development Life Cycle) to implement the chatbot web interface using Flask - Jinja2 backend development and React JS / CSS UI.",
+      "Fine-tuned the hyperparameters of the model to generate 50 different combinations of results, resulting in an impressive accuracy of 83%. This iterative optimization process significantly enhanced the performance and reliability of the model.",
+      "Performed A/B testing on the chatbot web UI, gathered user reviews and feedback, and implemented iterative improvements based on the results, resulting in a significant 25% increase in accuracy according to user feedback.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
